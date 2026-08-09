5c61cd078f15eae3a66547939a0231a738aacf21
ChristosKonstantas <titokonstantas1@gmail.com>
2026-08-09 15:51:24 +0200

    Added: header-only spsc queue

--- utils/queues/SpscQueue.hpp
+++ utils/queues/SpscQueue.hpp
@@ -3,7 +3,7 @@
 #include <array>
 #include <atomic>
 
-namespace lrs
+namespace anasa
 {
 
 // One producer and one consumer only.
@@ -12,13 +12,46 @@ template <class T, int capacity>
 class SpscQueue
 {
 public:
-    bool push(const T &item);
+    bool push(const T& item)
+    {
+        int write = _write.load(std::memory_order_relaxed);
+        int next = (write + 1) % capacity;
 
-    bool pop(T &item);
+        if (next == _read.load(std::memory_order_acquire))
+            return false;
 
-    bool peek(T &item) const;
+        _data[write] = item;
+        _write.store(next, std::memory_order_release);
+        return true;
+    }
 
-    int usableCapacity() const;
+    bool pop(T& item)
+    {
+        int read = _read.load(std::memory_order_relaxed);
+
+        if (read == _write.load(std::memory_order_acquire))
+            return false;
+
+        item = _data[read];
+        _read.store((read + 1) % capacity, std::memory_order_release);
+        return true;
+    }
+
+    bool peek(T& item) const
+    {
+        int read = _read.load(std::memory_order_relaxed);
+
+        if (read == _write.load(std::memory_order_acquire))
+            return false;
+
+        item = _data[read];
+        return true;
+    }
+
+    int usableCapacity() const
+    {
+        return capacity - 1;
+    }
 
 private:
     std::array<T, capacity> _data{};
@@ -26,4 +59,4 @@ private:
     std::atomic<int> _read{0};
 };
 
-} // namespace lrs
+} // namespace anasa
